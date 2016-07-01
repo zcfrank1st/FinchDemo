a7a3a61df5181ab536bff0b09a5478b94cc62e7c
zcfrank1st <zcfrank1st@sina.com>
2016-07-01 16:56:10 +0800

    add logger

--- build.sbt
+++ build.sbt
@@ -7,5 +7,7 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "com.github.finagle" %% "finch-core" % "0.10.0",
   "com.github.finagle" %% "finch-circe" % "0.10.0",
-  "io.circe" %% "circe-generic" % "0.3.0"
+  "io.circe" %% "circe-generic" % "0.3.0",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.4.0",
+  "ch.qos.logback" %  "logback-classic" % "1.1.7"
 )
--- src/main/scala-2.11/com/chaos/finch/demo/Main.scala
+++ src/main/scala-2.11/com/chaos/finch/demo/Main.scala
@@ -2,13 +2,14 @@ package com.chaos.finch.demo
 
 import com.twitter.finagle.Http
 import com.twitter.util.Await
+import com.typesafe.scalalogging.{LazyLogging, StrictLogging}
 import io.finch._
 import io.finch.circe._
 import io.circe.generic.auto._
 /**
   * Created by zcfrank1st on 7/1/16.
   */
-object Main extends App {
+object Main extends App with StrictLogging {
 
   case class Message(message: String)
 
@@ -21,6 +22,8 @@ object Main extends App {
     get("test" :: string("value")) { (value: String) =>
       Ok(Message(value))
     }
+
+  logger.info("server start !!!")
 
   Await.ready(
     Http
