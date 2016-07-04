2e9dcf8565ffbbdac5cc0d6a6e05570c9cfed6a1
zcfrank1st <zcfrank1st@sina.com>
2016-07-04 11:39:25 +0800

    use module to run rest service

--- src/main/scala-2.11/com/chaos/finch/demo/Main.scala
+++ src/main/scala-2.11/com/chaos/finch/demo/Main.scala
@@ -1,10 +1,10 @@
 package com.chaos.finch.demo
 
-import com.chaos.finch.demo.util.{ConfigModule, FinchModule}
+import com.chaos.finch.demo.util.FinchModule
 import com.twitter.server.TwitterServer
 
 
 /**
   * Created by zcfrank1st on 7/1/16.
   */
-object Main extends TwitterServer with FinchModule with ConfigModule
+object Main extends TwitterServer with FinchModule
