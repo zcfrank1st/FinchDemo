21f417a0a46f0bf26943a43f185dbeefbed707fa
zcfrank1st <zcfrank1st@sina.com>
2016-07-04 09:34:15 +0800

    use module to run twitter server

--- src/main/scala-2.11/com/chaos/finch/demo/Main.scala
+++ src/main/scala-2.11/com/chaos/finch/demo/Main.scala
@@ -1,24 +1,10 @@
 package com.chaos.finch.demo
 
-import com.chaos.finch.demo.v1.Api
-import com.twitter.finagle.Http
+import com.chaos.finch.demo.util.FinchModule
 import com.twitter.server.TwitterServer
-import com.twitter.util.Await
 
 
 /**
   * Created by zcfrank1st on 7/1/16.
   */
-object Main extends TwitterServer {
-  def main() {
-    val server = Http.serve(":8080", Api.apiService)
-
-    onExit {
-      server.close()
-    }
-
-    Await.ready(
-      server
-    )
-  }
-}
+object Main extends TwitterServer with FinchModule
---
+++ src/main/scala-2.11/com/chaos/finch/demo/util/FinchModule.scala
@@ -0,0 +1,23 @@
+package com.chaos.finch.demo.util
+
+import com.chaos.finch.demo.v1.Api
+import com.twitter.finagle.Http
+import com.twitter.util.Await
+
+/**
+  * Created by zcfrank1st on 7/4/16.
+  */
+trait FinchModule {
+  app: com.twitter.app.App =>
+  premain {
+    println("[Finch] server is starting ...")
+
+    val server = Http.serve(":8080", Api.apiService)
+    onExit {
+      server.close()
+    }
+    Await.ready(
+      server
+    )
+  }
+}
